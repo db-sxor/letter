23c14ddc82f9883090bee2f124fef82988aa8b44
goqkTk <jty_419@naver.com>
2026-03-23 19:48:36 +0900

    카드 수정

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const data = [
-  { lines:[{text:"틀림없이 소중한 당신에게,", top:"8%", left:"10%", size:"3vw"},{text:"두려워하지 마세요", top:"16%", left:"10%", size:"5vw"},{text:"당신이 평화롭길 바랍니다", top:"24%", left:"10%", size:"5vw"},{text:"오늘도 내일도 앞으로도", top:"32%", left:"10%", size:"5vw"},{text:"힘을 내세요", top:"40%", left:"10%", size:"5vw"},{text:"그리고...", top:"48%", left:"10%", size:"5vw"},{text:"힘을 내세요", top:"56%", left:"10%", size:"5vw"}], extra:"\"매우 귀중한 사람이여,\n두려워하지 마시오. 당신에게 평화가 있기를 바라오.\n힘을 내시오. 힘을 내시오.\"\n- 다니엘 10:19" },
-  { lines:[{text:"내일의 일을 너무 염려하지 마세요", top:"8%", left:"10%", size:"5vw"},{text:"내일의 걱정은 내일해도 괜찮습니다", top:"16%", left:"10%", size:"5vw"},{text:"오늘은 오늘의 당신이 수고했으니,", top:"24%", left:"10%", size:"5vw"},{text:"이제부터는", top:"32%", left:"10%", size:"5vw"},{text:"편안한 마음을 간직하시면 좋겠습니다", top:"40%", left:"10%", size:"5vw"}], extra:"내일 일을 결코 염려하지 마십시오.\n내일은 내일의 염려가 있을 것입니다.\n그날의 괴로움은 그날로 충분합니다.\n- 마태복음 6:34" },
+  { lines:[{text:"틀림없이 소중한 당신에게,", top:"8%", left:"10%", size:"3vw"},{text:"두려워하지 마세요", top:"16%", left:"10%", size:"3vw"},{text:"당신이 평화롭길 바랍니다", top:"24%", left:"10%", size:"3vw"},{text:"오늘도 내일도 앞으로도", top:"32%", left:"10%", size:"3vw"},{text:"힘을 내세요", top:"40%", left:"10%", size:"3vw"},{text:"그리고...", top:"48%", left:"10%", size:"3vw"},{text:"힘을 내세요", top:"56%", left:"10%", size:"3vw"}], extra:"\"매우 귀중한 사람이여, 두려워하지 마시오.\n당신에게 평화가 있기를 바라오.\n힘을 내시오. 힘을 내시오.\"\n- 다니엘 10:19" },
+  { lines:[{text:"내일의 일을 너무 염려하지 마세요", top:"8%", left:"10%", size:"3vw"},{text:"내일의 걱정은 내일해도 괜찮습니다", top:"16%", left:"10%", size:"3vw"},{text:"오늘은 오늘의 당신이 수고했으니,", top:"24%", left:"10%", size:"3vw"},{text:"이제부터는", top:"32%", left:"10%", size:"3vw"},{text:"편안한 마음을 간직하시면 좋겠습니다", top:"40%", left:"10%", size:"3vw"}], extra:"내일 일을 결코 염려하지 마십시오.\n내일은 내일의 염려가 있을 것입니다.\n그날의 괴로움은 그날로 충분합니다.\n- 마태복음 6:34" },
   { lines:[{text:"요즘은 이상하게", top:"22%", left:"10%", size:"3vw"},{text:"조용해지고 싶다.", top:"40%", left:"10%", size:"3vw"}], extra:"혼자 있는 시간이 필요하다." },
   { lines:[{text:"오늘 하루는", top:"20%", left:"10%", size:"3vw"},{text:"괜찮았다.", top:"36%", left:"10%", size:"3vw"}], extra:"작은 일들이 쌓였다." },
   { lines:[{text:"괜히 웃음이 나왔다.", top:"30%", left:"10%", size:"3vw"}], extra:"이유 없어도 괜찮다." },
@@ -38,8 +38,10 @@ function createCard(item){
     const div = document.createElement('div');
     div.textContent = text;
 
-    if(i === lines.length - 1) div.style.textAlign = 'right';
-
+    if(i === lines.length - 1) {
+      div.style.alignSelf = 'flex-end';
+      div.style.textAlign = 'right';
+    }
     overlay.appendChild(div);
   });
 
